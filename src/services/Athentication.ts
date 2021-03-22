f8bd9457e7bb879686c72ae493f47ff3aa683cad
kovacevic <kovacevic@tree3.services>
2021-03-22 15:28:45 +0200

    added errors

--- src/services/Athentication.ts
+++ src/services/Athentication.ts
@@ -1,9 +1,14 @@
 import { LoginResult, Errors } from './dtos';
 
-export interface AthenticationService {
+export interface AuthenticationService {
   login(
     username: string,
     password: string
-  ): Promise<LoginResult | Errors.UsernameNotFound | Errors.WrongPassword>;
-  logout(): Promise<void | Errors.Unexpected>;
+  ): Promise<
+    | LoginResult
+    | Errors.UsernameNotFound
+    | Errors.WrongPassword
+    | Errors.Unexpected
+  >;
+  logout(token: string): Promise<void | Errors.Unexpected>;
 }
