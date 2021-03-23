330a7e0cbf044f505f0c32d82c96568d7868ad8d
kovacevic <kovacevic@tree3.services>
2021-03-23 10:24:28 +0200

    added mock authentication

---
+++ src/services/mock/Authentication.ts
@@ -0,0 +1,52 @@
+import { AuthenticationService } from '../Athentication';
+import { LoginResult, NewUser, Errors } from '../dtos';
+import { User, UserRole } from '@/types';
+import JWTEncode from 'jwt-encode';
+
+const secret = '122345';
+
+const userStore = new Map<string /* username */, NewUser>();
+
+const testUser: NewUser = {
+  username: 'test',
+  fullName: 'test test',
+  email: 'test@test.com',
+  password: 'test',
+  tel1: '123456789',
+  dateOfBirth: new Date(Date.now()),
+  roles: [UserRole.admin],
+};
+
+userStore.set(testUser.username, testUser);
+
+export default class MockAuthenticationService
+  implements AuthenticationService {
+  async login(
+    username: string,
+    password: string
+  ): Promise<LoginResult | Errors.UsernameNotFound | Errors.WrongPassword> {
+    const user: NewUser | undefined = userStore.get(username);
+
+    if (user == undefined) {
+      return Errors.UsernameNotFound;
+    } else if (user.password !== password) {
+      return Errors.WrongPassword;
+    }
+
+    const tokenPayload = {
+      id: 'test',
+      role: UserRole.admin,
+      iat: 'idk',
+      exp: '',
+    };
+    const encodedToken = JWTEncode(tokenPayload, secret);
+    const res: LoginResult = {
+      token: encodedToken,
+    };
+    return res;
+  }
+
+  async logout(): Promise<void | Errors.Unexpected> {
+    return;
+  }
+}
