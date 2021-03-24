2be7db8952348001c59f9d314a54b7992e1a55ba
kovacevic <kovacevic@tree3.services>
2021-03-24 09:47:21 +0200

    added login vue

---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="">
+    <h2>Login</h2>
+    <div>
+      <label for="username">Username</label>
+      <input
+        data-test="username"
+        type="text"
+        id="username"
+        name="username"
+        v-model="credentials.username"
+      />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input
+        data-test="password_input"
+        type="password"
+        id="password"
+        name="password"
+        v-model="credentials.password"
+      />
+    </div>
+    <button type="submit" data-test="submit_button" @click="login()">
+      Login
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useAuth } from '@/composables/useAuth';
+
+export default defineComponent({
+  name: 'Login',
+  setup() {
+    const { credentials, login } = useAuth();
+
+    return {
+      login,
+      credentials,
+    };
+  },
+});
+</script>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
