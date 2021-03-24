99ac39de9fce23b21eb63d691c4907ba047e4f9a
kovacevic <kovacevic@tree3.services>
2021-03-24 09:39:29 +0200

    added route guard

---
+++ src/router/guards.ts
@@ -0,0 +1,27 @@
+import { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
+
+import { container } from 'tsyringe';
+
+import AuthStore from '@/stores/authStore';
+
+export const authenticatedGuard = (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  const authStore = container.resolve(AuthStore);
+
+  if (to.name !== 'login') {
+    if (!authStore.IsAthenticated()) {
+      return next({ name: 'login' });
+    } else {
+      return next();
+    }
+  } else {
+    if (authStore.IsAthenticated()) {
+      return next({ name: 'home' });
+    } else {
+      return next();
+    }
+  }
+};
