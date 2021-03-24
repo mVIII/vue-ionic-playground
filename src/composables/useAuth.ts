56b87a5b53d5fd13d36f657fc3700747cadc8d79
kovacevic <kovacevic@tree3.services>
2021-03-24 09:39:20 +0200

    added auth composable

---
+++ src/composables/useAuth.ts
@@ -0,0 +1,29 @@
+import { container } from 'tsyringe';
+import AuthStore from '@/stores/authStore';
+import { reactive } from 'vue';
+
+export function useAuth() {
+  const authStore = container.resolve(AuthStore);
+
+  const credentials = reactive({
+    username: '',
+    password: '',
+  });
+
+  async function login(): Promise<void> {
+    const error = await authStore.Login(
+      credentials.username,
+      credentials.password
+    );
+
+    if (error) {
+      throw error;
+    }
+    //window.location.reload();
+  }
+
+  return {
+    login,
+    credentials,
+  };
+}
