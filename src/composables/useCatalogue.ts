2ce5ae6ff73c8ef3380543856f647ad3f8ba09a6
kovacevic <kovacevic@tree3.services>
2021-03-25 21:13:53 +0200

    implemented fitlering

--- src/composables/useCatalogue.ts
+++ src/composables/useCatalogue.ts
@@ -2,11 +2,16 @@ import { CatalogueService } from '@/services/Catalogue';
 import { Errors } from '@/services/dtos';
 import { Catalogue, Item } from '@/types';
 import { container } from 'tsyringe';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 export function useCatalogue() {
+  const selectedCatalogue = ref<Catalogue>({
+    id: '',
+    name: '',
+    items: [],
+  });
   const catalogues = ref<Catalogue[]>([]);
-  const items = ref<Item[]>([]);
+  const displayedItems = ref<Item[]>([]);
 
   const catalogueService = container.resolve<CatalogueService>('Catalogue');
 
@@ -19,20 +24,37 @@ export function useCatalogue() {
     catalogues.value = result;
   }
 
-  async function getCatalogueItems(id: string) {
-    const result = await catalogueService.GetCatalogueItems(id);
+  async function getCatalogue(id: string) {
+    const result = await catalogueService.GetCatalogue(id);
     if (result === Errors.Unexpected) {
       throw result;
     } else if (result === Errors.CatalogueNotFound) {
       throw result;
     }
-    items.value = result;
+    selectedCatalogue.value = result;
   }
 
+  function filterDisplayedItems(query: string) {
+    console.log(query)
+    const itemsToDisplay = selectedCatalogue.value.items
+      .map((item) =>
+        item.name.toLowerCase().indexOf(query.toLocaleLowerCase()) > -1 ? item : null
+      )
+      .filter((v) => v) as Item[];
+      console.log(itemsToDisplay)
+    displayedItems.value = itemsToDisplay;
+  }
+
+  watch(selectedCatalogue, () => {
+    displayedItems.value = selectedCatalogue.value.items;
+  });
+
   return {
-    items,
+    displayedItems,
+    filterDisplayedItems,
+    selectedCatalogue,
     catalogues,
-    getCatalogueItems,
     getCatalogues,
+    getCatalogue,
   };
 }
