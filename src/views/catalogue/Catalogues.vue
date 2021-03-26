aecc8ea328a74ec1380c657d624361a5e9d5f84f
kovacevic <kovacevic@tree3.services>
2021-03-26 11:47:26 +0200

    added loading and refresh

--- src/views/catalogue/Catalogues.vue
+++ src/views/catalogue/Catalogues.vue
@@ -6,9 +6,21 @@
         <ion-buttons slot="start">
           <ion-menu-button></ion-menu-button>
         </ion-buttons>
+        <ion-buttons slot="end">
+          <ion-button @click="setOptionsOpen(true)">
+            <ion-icon
+              :icon="ellipsisVertical"
+              :ios="ellipsisHorizontal"
+              :md="ellipsisVertical"
+            />
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+      <ion-refresher slot="fixed" @ionRefresh="refreshCatalogues($event)">
+        <ion-refresher-content></ion-refresher-content>
+      </ion-refresher>
       <ion-list>
         <ion-item
           v-for="catalogue in catalogues"
@@ -22,6 +34,18 @@
         </ion-item>
       </ion-list>
     </ion-content>
+    <ion-loading
+      :is-open="cataloguesLoading"
+      message="Please wait..."
+      :duration="1000"
+    >
+    </ion-loading>
+    <ion-action-sheet
+      :is-open="optionsOpen"
+      header="Catalogues"
+      :buttons="optionButtons"
+    >
+    </ion-action-sheet>
   </ion-page>
 </template>
 
@@ -38,15 +62,22 @@ import {
   IonMenuButton,
   IonButtons,
   IonTitle,
+  IonLoading,
+  IonRefresher,
+  IonIcon,
+  IonRefresherContent,
+  IonButton,
+  IonActionSheet,
 } from '@ionic/vue';
-
 import { useCatalogue } from '@/composables/useCatalogue';
 import { onMounted } from '@vue/runtime-core';
 import router from '@/router';
-
+import { book, ellipsisVertical, ellipsisHorizontal } from 'ionicons/icons';
+import { ref } from 'vue';
 export default {
   name: 'Catalogues',
   components: {
+    IonButton,
     IonButtons,
     IonTitle,
     IonLabel,
@@ -58,6 +89,11 @@ export default {
     IonContent,
     IonPage,
     IonMenuButton,
+    IonLoading,
+    IonRefresher,
+    IonIcon,
+    IonRefresherContent,
+    IonActionSheet,
   },
   methods: {
     goToCatalogue(id: string) {
@@ -65,18 +101,72 @@ export default {
     },
   },
   setup() {
-    const { getCatalogues, catalogues } = useCatalogue();
+    const {
+      refreshCatalogues,
+      catalogues,
+      cataloguesLoading,
+      runWrappedGetCatalogues,
+    } = useCatalogue();
+
+    const optionsOpen = ref(false);
+    const setOptionsOpen = (state: boolean) => (optionsOpen.value = state);
 
     onMounted(async () => {
       try {
-        await getCatalogues();
-        console.log('ready');
+        await runWrappedGetCatalogues();
       } catch (error) {
         console.log(error);
       }
     });
 
-    return { catalogues };
+    type Button = {
+      text: string;
+      role?: string;
+      icon?: string;
+      handler: Function;
+    };
+
+    const optionButtons: Button[] = [
+      {
+        text: 'Add',
+        handler: () => {
+          console.log('Add');
+        },
+      },
+      {
+        text: 'Edit',
+        handler: () => {
+          console.log('Delete');
+        },
+      },
+      {
+        text: 'Delete',
+        role:"destructive",
+        handler: () => {
+          console.log('Delete');
+        },
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          setOptionsOpen(false);
+          console.log('Cancel');
+        },
+      },
+    ];
+
+    return {
+      catalogues,
+      cataloguesLoading,
+      refreshCatalogues,
+      book,
+      ellipsisHorizontal,
+      ellipsisVertical,
+      optionButtons,
+      optionsOpen,
+      setOptionsOpen,
+    };
   },
 };
 </script>
