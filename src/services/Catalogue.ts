bca0c5b0be7ad7ab62e11a3aa5b079d771a16054
kovacevic <kovacevic@tree3.services>
2021-03-29 09:58:57 +0300

    changed Catalogue service specification

--- src/services/Catalogue.ts
+++ src/services/Catalogue.ts
@@ -3,7 +3,9 @@ import { Errors } from './dtos';
 
 export interface CatalogueService {
   CreateCatalogue(catalogue: Catalogue): Promise<Catalogue | Errors.Unexpected>;
-  GetCatalogues(): Promise<Catalogue[] | Errors.Unexpected>;
+  GetCatalogues(
+    page: number,
+  ): Promise<Catalogue[] | Errors.Unexpected>;
   GetCatalogue(
     id: string
   ): Promise<Catalogue | Errors.Unexpected | Errors.CatalogueNotFound>;
