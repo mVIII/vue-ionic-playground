45744b17ee2cf7cc9691de89a85921d159093e39
kovacevic <kovacevic@tree3.services>
2021-04-01 12:10:38 +0300

    added custom fields

--- src/views/catalogue/NewEdit.vue
+++ src/views/catalogue/NewEdit.vue
@@ -25,6 +25,69 @@
           v-model="c.description"
         ></ion-input>
       </ion-item>
+      <ion-toolbar>
+        <ion-list-header>
+          Fields
+          <ion-fab>
+            <ion-fab-button @click="clickedAddField()">
+              <ion-icon :icon="add"></ion-icon>
+            </ion-fab-button>
+          </ion-fab>
+        </ion-list-header>
+      </ion-toolbar>
+      <ion-item v-for="field in c.ItemSchema" :key="field.id">
+        <ion-list>
+          <ion-toolbar>
+            <ion-list-header>
+              {{ field.name }}
+            </ion-list-header>
+            <ion-buttons slot="end">
+              <ion-button @click="clickedOptions(catalogue)">
+                <ion-icon
+                  :icon="ellipsisVertical"
+                  :ios="ellipsisHorizontal"
+                  :md="ellipsisVertical"
+                />
+              </ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+          <ion-item>
+            <ion-label>Name</ion-label>
+            <ion-input
+              placeholder="Field name"
+              v-model="field.name"
+              slot="end"
+            ></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-label>Type</ion-label>
+            <ion-select interface="popover" v-model="field.type" slot="end">
+              <ion-select-option value="0">Alpharithemtic</ion-select-option>
+              <ion-select-option value="1">Number</ion-select-option>
+              <ion-select-option value="2">Enum</ion-select-option>
+              <ion-select-option value="3">Boolean</ion-select-option>
+            </ion-select>
+          </ion-item>
+          <ion-item>
+            <ion-label>Required</ion-label>
+            <ion-toggle
+              color="primary"
+              v-model="field.required"
+              slot="end"
+            ></ion-toggle>
+          </ion-item>
+          <ion-item v-if="field.type == 2">
+            <ion-label position="stacked">Values</ion-label>
+            <ion-textarea>
+              <ion-chip v-for="enumVal in field.enum" :key="enumVal">
+                <ion-label>{{ enumVal }}</ion-label>
+                <ion-icon :icon="close"></ion-icon>
+              </ion-chip>
+            </ion-textarea>
+          </ion-item>
+        </ion-list>
+      </ion-item>
+      <ion-item-divider></ion-item-divider>
     </ion-list>
     <ion-button expand="block" @click="$emit('save', c)">Add</ion-button>
   </ion-content>
@@ -43,12 +106,26 @@ import {
   IonItem,
   IonLabel,
   IonButton,
+  IonSelect,
+  IonSelectOption,
+  IonChip,
+  IonTextarea,
+  IonIcon,
+  IonItemDivider,
+  IonToggle,
+  IonFabButton,
+  IonFab,
 } from '@ionic/vue';
 
-import { Catalogue } from '@/types';
+import { Catalogue, FieldTypes } from '@/types';
 import { PropType } from '@vue/runtime-core';
 import { ref } from 'vue';
-
+import {
+  close,
+  add,
+  ellipsisVertical,
+  ellipsisHorizontal,
+} from 'ionicons/icons';
 export default {
   name: 'CatalogueNewEdit',
   components: {
@@ -63,6 +140,15 @@ export default {
     IonItem,
     IonLabel,
     IonButton,
+    IonSelect,
+    IonSelectOption,
+    IonChip,
+    IonTextarea,
+    IonIcon,
+    IonItemDivider,
+    IonToggle,
+    IonFabButton,
+    IonFab,
   },
   emits: ['dismiss', 'save'],
   props: {
@@ -75,10 +161,32 @@ export default {
     const title =
       props.catalogue.name === '' ? 'New Catalogue' : props.catalogue.name;
     const c = ref<Catalogue>(props.catalogue);
+
+    const clickedAddField = () => {
+      c.value.ItemSchema.unshift({
+        id: '0',
+        name: 'New Field',
+        type: FieldTypes.String,
+        required: false,
+      });
+    };
+
     return {
+      clickedAddField,
+      ellipsisVertical,
+      ellipsisHorizontal,
+      add,
+      close,
       c,
       title,
     };
   },
 };
 </script>
+<style scoped>
+ion-fab-button {
+  margin-left: 70px;
+  width: 23px;
+  height: 23px;
+}
+</style>
