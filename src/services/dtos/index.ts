7641ddcc2cb0e40be3f1c66065a21d531ef0f9da
kovacevic <kovacevic@tree3.services>
2021-04-02 16:53:01 +0300

    exported notification types

--- src/services/dtos/index.ts
+++ src/services/dtos/index.ts
@@ -9,3 +9,4 @@ export {
 export { ItemPage, ItemDTO } from './item';
 export { LoginResult, NewUser } from './authentication';
 export { Pagination } from './pagination';
+export { Notification, NotificationCallback } from './notifications';
