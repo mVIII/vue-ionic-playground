0ad29a13c28102dc5e7b83505d323fdf80a38003
kovacevic <kovacevic@tree3.services>
2021-04-02 14:32:06 +0300

    added ItemList && ItemGrid Component

---
+++ src/components/ItemGrid.vue
@@ -0,0 +1,57 @@
+<template>
+  <ion-grid>
+    <ion-col>
+      <ion-row>
+        <ion-card v-for="item in items" :key="item.name">
+          <ion-card-content>
+            <div style="display: flex; justify-content: space-evenly">
+              <ion-avatar slot="start">
+                <img :src="item.image" />
+              </ion-avatar>
+            </div>
+            <strong>{{ item.name }}</strong>
+            <br />
+            <strong>{{ item.description }}</strong>
+            <br />
+          </ion-card-content>
+        </ion-card>
+      </ion-row>
+    </ion-col>
+  </ion-grid>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { Item } from '@/types';
+import { PropType } from '@vue/runtime-core';
+import {
+  IonCardContent,
+  IonAvatar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+} from '@ionic/vue';
+export default defineComponent({
+  name: 'ItemGrid',
+  components: {
+    IonCard,
+    IonGrid,
+    IonCol,
+    IonRow,
+    IonCardContent,
+    IonAvatar,
+  },
+  props: {
+    displayItems: {
+      type: Object as PropType<Item[]>,
+      default: [],
+    },
+  },
+  setup(props: any) {
+    const items = ref<Item[]>(props.displayItems);
+    console.log(items.value);
+    return { items };
+  },
+});
+</script>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-list>
+    <ion-item v-for="item in items" :key="item.name">
+      <ion-avatar slot="start">
+        <img :src="item.image" />
+      </ion-avatar>
+      <ion-label>
+        <h2>{{ item.name }}</h2>
+        <p>{{ item.description }}</p>
+      </ion-label>
+    </ion-item>
+  </ion-list>
+</template>
+<script lang="ts">
+import { Item } from '@/types';
+import { PropType } from '@vue/runtime-core';
+import { defineComponent, ref } from 'vue';
+import { IonList, IonItem, IonLabel, IonAvatar } from '@ionic/vue';
+
+export default defineComponent({
+  name: 'ItemList',
+  components: {
+    IonAvatar,
+    //IonListHeader,
+    IonList,
+    IonItem,
+    IonLabel,
+  },
+  props: {
+    displayItems: {
+      type: Object as PropType<Item[]>,
+      default: [],
+    },
+  },
+  setup(props: any) {
+    const items = ref<Item[]>(props.displayItems);
+    return { items };
+  },
+});
+</script>
